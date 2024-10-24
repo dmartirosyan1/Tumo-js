cbe9fc462caed246a763693fec9c74205d7f9e1b
dmartirosyan1 <diana.martirosyan1@tumo.org>
2024-10-25 00:20:48 +0400

    TumoJS

--- script.js
+++ script.js
@@ -7,7 +7,50 @@ width(8)
 rotate(60)
 drawEllipse(120, 80);
 erase()
+goTo(470,180)
+width(10)
+rotate(20)
 drawRectangle(120,100)
+goTo(370,280)
+width(3)
+rotate(0)
+drawEllipse(90)
+showGrid(10)
+right(90)
+// drawLine(120)
+fillColor('black')
+goTo(170,280)
+drawStar(6, 100)
+goTo(116,119)
+fillColor('red')
+rotate(60)
+drawTriangle(100)
+goTo(570,380)
+width(6)
+drawPolygon(5,60)
+width(2)
+color('green')
+fillColor('purple')
+rotate(0)
+goTo(690,141)
+drawEllipse(40, 50)
+goTo(240,430)
+rotate(60)
+drawRectangle(70)
+goTo(280,30)
+color('blue')
+text('hello',56)
+goTo(70,70)
+fillColor('yellow')
+color('yellow')
+drawEllipse(50)
+goTo(630,256)
+fillColor('lightBlue')
+color('lightBlue')
+drawPolygon(9, 40)
+
+
+
 
 // goTo(100, 100);
 // fillColor(120, 19, 149);
@@ -20,7 +63,7 @@ drawRectangle(120,100)
 // goTo(700, 500);
 // drawEllipse(80);
 // fillColor("red");
-// erase();
+// // erase();
 // drawLine(120);
 // drawEllipse(140, 100);
 // drawLine(50);
